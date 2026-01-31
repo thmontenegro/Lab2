55e40532f10a8c1f3c8a46cf9e1f7f22d63e59a2
thmontenegro <thelmamontenegro2015@gmail.com>
2026-01-30 20:59:38 -0600

    phase2

--- rn-viewsimproved/App.js
+++ rn-viewsimproved/App.js
@@ -1,30 +1,56 @@
-import { useState } from "react";
-import { View, Text, TextInput, Button, ScrollView } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TextInput, KeyboardAvoidingView, Platform, StyleSheet } from "react-native";
+import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   const [task, setTask] = useState("");
   const [tasks, setTasks] = useState([]);
 
-  const addTask = () => {
-    setTasks([...tasks, task]); 
-    setTask("");
-  };
-
   return (
-    <View style={{ flex: 1, paddingTop: 50 }}>
-      <Text style={{ fontSize: 30, textAlign: 'center' }}>To-Do List</Text>
-      <ScrollView>
-        {tasks.map((item, index) => (
-          <Text key={index} style={{ fontSize: 20, padding: 10 }}>* {item}</Text>
-        ))}
-      </ScrollView>
-      <TextInput
-        placeholder="New Task"
-        value={task}
-        onChangeText={setTask}
-        style={{ borderWidth: 1, margin: 20, padding: 10 }}
-      />
-      <Button title="Add" onPress={addTask} />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <KeyboardAvoidingView 
+          style={{ flex: 1 }}
+          behavior={Platform.OS === "ios" ? "padding" : "height"} 
+        >
+          <Text style={styles.title}>To-Do</Text>
+          <View style={{ flex: 1 }}>
+            {tasks.map((t, i) => <Text key={i} style={styles.task}>{t}</Text>)}
+          </View>
+          <TextInput
+            style={styles.input}
+            value={task}
+            onChangeText={setTask}
+            onEndEditing={() => {
+              setTasks([...tasks, task]);
+              setTask("");
+            }}
+          />
+        </KeyboardAvoidingView>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { 
+    flex: 1, 
+    backgroundColor: "white" 
+  },
+  title: { 
+    fontSize: 36, 
+    fontWeight: 'bold', 
+    alignSelf: "center" 
+  },
+  input: { 
+    borderBottomWidth: 1, 
+    borderColor: "#d3d3d3", 
+    width: "90%", 
+    padding: 10, 
+    alignSelf: 'center'
+  },
+  task: { 
+    fontSize: 20, 
+    padding: 10 
+  }
+});
